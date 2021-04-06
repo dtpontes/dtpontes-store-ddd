e1bdd66457d017db58eeacdaccf9f1d4373e5bc3
Daniel <dtpontesinfo@gmail.com>
2021-04-06 18:54:28 -0300

    Criação da entidade Produto e categoria

---
+++ src/DtpontesStore.Catalogo.Domain/Produto.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DtpontesStore.Core.DomainObjects;
+
+namespace DtpontesStore.Catalogo.Domain
+{
+    public class Produto : Entity, IAggregateRoot
+    {
+        public Produto(Guid categoriaId, string nome, string descricao, bool ativo, decimal valor, DateTime dataCadastro, string imagem)
+        {
+            CategoriaId = categoriaId;
+            Nome = nome;
+            Descricao = descricao;
+            Ativo = ativo;
+            Valor = valor;
+            DataCadastro = dataCadastro;
+            Imagem = imagem;
+        }
+
+        public Guid CategoriaId { get; private set; }
+        public string Nome { get; private set; }
+
+        public string Descricao { get; private set; }
+
+        public bool Ativo { get; private set; }
+
+        public decimal Valor { get; private set; }
+
+        public DateTime DataCadastro { get; private set; }
+
+        public string Imagem { get; private set; }
+
+        public int QuantidadeEstoque { get; private set; }
+
+        public Categoria Categoria { get; private set; }
+
+        public void Ativar() => Ativo = true;
+
+        public void Desativar() => Ativo = false;
+
+        public void AlterarCategoria(Categoria categoria)
+        {
+            Categoria = categoria;
+            CategoriaId = categoria.Id;
+        }
+
+        public void AlterarDescricao(string descricao)
+        {
+            Descricao = descricao;
+        }
+
+        public void DebitarEstoque(int quantidade)
+        {
+            if (quantidade < 0) quantidade *= -1;
+            QuantidadeEstoque -= quantidade;
+        }
+
+        public void ReporEstoque(int quantidade)
+        {            
+            QuantidadeEstoque += quantidade;
+        }
+
+        public bool PossuiEstoque(int quantidade)
+        {
+            
+            return QuantidadeEstoque >= quantidade;
+        }
+
+        public void Validar()
+        {
+
+        }
+
+    }
+
+    public class Categoria : Entity
+    {
+        public Categoria(string nome, int codigo)
+        {
+            Nome = nome;
+            Codigo = codigo;
+        }
+
+        public string Nome { get; private set; }
+
+        public int Codigo { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{Nome} - {Codigo}";
+        }
+
+    }
+}
---
+++ src/DtpontesStore.Core/DomainObjects/IAggregateRoot.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DtpontesStore.Core.DomainObjects
+{
+    public interface IAggregateRoot
+    {
+    }
+}
