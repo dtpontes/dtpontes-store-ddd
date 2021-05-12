ca5db7549a6af1233f2a312bad31575d93aeacde
Daniel <dtpontesinfo@gmail.com>
2021-05-11 23:40:02 -0300

    Implementando a camada de application do Catalogo

---
+++ src/DtpontesStore.Catalogo.Application/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using DtpontesStore.Catalogo.Application.ViewModels;
+using DtpontesStore.Catalogo.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DtpontesStore.Catalogo.Application.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public DomainToViewModelMappingProfile()
+        {
+            CreateMap<Produto, ProdutoViewModel>()
+                .ForMember(d => d.Largura, o => o.MapFrom(s => s.Dimensoes.Largura))
+                .ForMember(d => d.Altura, o => o.MapFrom(s => s.Dimensoes.Altura))
+                .ForMember(d => d.Profundidade, o => o.MapFrom(s => s.Dimensoes.Profundidade));
+
+            CreateMap<Categoria, CategoriaViewModel>();
+        }
+    }
+}
---
+++ src/DtpontesStore.Catalogo.Application/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using DtpontesStore.Catalogo.Application.ViewModels;
+using DtpontesStore.Catalogo.Domain;
+
+namespace DtpontesStore.Catalogo.Application.AutoMapper
+{
+    public class ViewModelToDomainMappingProfile : Profile
+    {
+        public ViewModelToDomainMappingProfile()
+        {
+            CreateMap<ProdutoViewModel, Produto>()
+                .ConstructUsing(c => new Produto(c.Id, c.Nome, c.Descricao,c.Ativo, c.Valor,c.DataCadastro, c.Imagem, new Dimensoes(c.Altura, c.Largura,c.Profundidade)));
+
+            CreateMap<CategoriaViewModel, Categoria>()
+                .ConstructUsing(c=>new Categoria(c.Nome, c.Codigo));
+        }
+    }
+}
